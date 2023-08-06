2cee223794665bf397e1a9f0141cf30822d3b8ca
Thomas J. Fan <thomasjpfan@gmail.com>
2023-08-06 17:28:52 -0400

    REL Init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# scikit-learn/scikit-learn: Pull Requests Overview
+
+## License
+
+This repo is under the [MIT License](LICENSE).
---
+++ build.py
@@ -0,0 +1,125 @@
+import argparse
+import requests
+import sys
+import os
+import string
+import json
+import pandas as pd
+from io import StringIO
+from datetime import datetime, timezone
+
+REPO_NAME = "scikit-learn"
+REPO_OWNER = "scikit-learn"
+TRACKER_REPO = "thomasjpfan/sklearn-pr-tracker"
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", default="")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--save_file", help="Set to file to save results locally")
+parser.add_argument("--load_file", help="Set to file to load results locally")
+args = parser.parse_args()
+
+query_template = string.Template(
+    """
+query {
+  repository(name:"$REPO_NAME", owner:"$REPO_OWNER") {
+    pullRequests(
+      first: 100,
+      after: $CURSOR,
+      states: OPEN
+    ) {
+        nodes {
+        title
+        updatedAt
+        additions
+        deletions
+        number
+        totalCommentsCount
+        reviews(last: 1) {
+          totalCount
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}
+"""
+)
+
+
+if args.load_file:
+    print("Loading data locally")
+    with open(args.load_file, "r") as f:
+        results = json.load(f)
+
+else:
+    print("Getting data from GitHub")
+    url = "https://api.github.com/graphql"
+
+    if not GITHUB_TOKEN:
+        print("A GITHUB_TOKEN must be defined to query the GraphQL")
+        sys.exit(1)
+    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+
+    has_next_page = True
+    cursor = "null"
+    results = []
+    while has_next_page:
+        query = query_template.substitute(
+            CURSOR=cursor, REPO_NAME=REPO_NAME, REPO_OWNER=REPO_OWNER
+        )
+        r = requests.post(url=url, json={"query": query}, headers=headers)
+        r_json = r.json()
+
+        page_info = r_json["data"]["repository"]["pullRequests"]["pageInfo"]
+        has_next_page = page_info["hasNextPage"]
+        if has_next_page:
+            cursor = f'"{page_info["endCursor"]}"'
+        results.extend(r_json["data"]["repository"]["pullRequests"]["nodes"])
+
+        print(f"Current number of results: {len(results)}")
+
+    if args.save_file:
+        with open(args.save_file, "w") as f:
+            json.dump(results, f)
+
+
+print("Processing results")
+results_processed = [
+    {
+        "pr": result["number"],
+        "title": f'<a href="https://github.com/scikit-learn/scikit-learn/pull/{result["number"]}">{result["title"]}</a>',
+        "additions": result["additions"],
+        "deletions": result["deletions"],
+        "comments": result["totalCommentsCount"],
+        "reviews": result["reviews"]["totalCount"],
+        "updated": result["updatedAt"],
+    }
+    for result in results
+]
+
+results_df = pd.DataFrame.from_records(results_processed)
+results_df["updated"] = pd.to_datetime(results_df["updated"])
+
+buffer = StringIO()
+results_df.to_csv(buffer, index=False)
+result_str = buffer.getvalue().replace("`", "\`")
+
+print("Creating index html page")
+with open("index.html.template", "r") as f:
+    template = string.Template(f.read())
+
+utc_now = datetime.now(timezone.utc)
+
+output = template.substitute(
+    CSV_CONTENT=result_str,
+    TRACKER_REPO=TRACKER_REPO,
+    TARGET_REPO=f"{REPO_OWNER}/{REPO_NAME}",
+    DATE=utc_now.strftime("%B %d, %Y"),
+)
+
+print("Writing to build/index.html")
+with open("build/index.html", "w") as f:
+    f.write(output)
